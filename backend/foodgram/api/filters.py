135158fbc086f61782a98510b6ec6af79f35b729
Алексей Плотников <abc-007@yandex.ru>
2023-04-17 22:17:01 +0500

    filterV1

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -6,8 +6,12 @@ from recipes.models import Recipe
 
 
 class RecipeFilter(django_filters.FilterSet):
-    tags = django_filters.CharFilter(
-        field_name='tags__slug', method='filter_tags', lookup_expr='in')
+    tags = django_filters.ModelMultipleChoiceFilter(
+        name='tags__slug',
+        to_field_name='name',
+        lookup_type='in',
+        queryset=Recipe.objects.all()
+        )
     is_in_shopping_cart = django_filters.BooleanFilter(
         field_name='is_in_shopping_cart', method='filter_is_in_shopping_cart')
     is_favorited = django_filters.BooleanFilter(
